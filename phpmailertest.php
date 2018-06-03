a4bac27c2e6c607bfe0f3cf08924b324b30c72b5
Ronald <ronald@mail.Lenovo-Ronald-Ubuntu>
2018-06-03 23:04:28 +0200

    phpmailer.min test

---
+++ phpmailertest.php
@@ -0,0 +1,33 @@
+<?php
+date_default_timezone_set('Etc/UTC');
+
+require 'phpmailer.min/class.phpmailer.php';
+require 'phpmailer.min/SMTP.php';
+
+$mail = new PHPMailer;
+$mail->isSMTP();
+
+//Enable SMTP debugging
+// 0 = off (for production use)
+// 1 = client messages
+// 2 = client and server messages
+$mail->SMTPDebug = 2;
+
+$mail->Host = 'localhost';
+$mail->Port = 25;
+$mail->SMTPAuth = false;
+
+$mail->setFrom('from@example.com', 'First Last');
+$mail->addReplyTo('replyto@example.com', 'First Last');
+$mail->addAddress('whoto@example.com', 'John Doe');
+$mail->Subject = 'PHPMailer SMTP test';
+
+$mail->msgHTML(file_get_contents('phpmailer.min/contents.html'), __DIR__);
+$mail->AltBody = 'This is a plain-text message body';
+$mail->addAttachment('phpmailer.min/images/phpmailer_mini.png');
+
+if (!$mail->send()) {
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message sent!';
+}
