a62e93e1e6925174e1dbba598af301cfa851bbe8
Ronald <ronald@mail.Lenovo-Ronald-Ubuntu>
2018-06-04 22:49:37 +0200

    Properties moved from server.php to class

--- server.php
+++ server.php
@@ -2,11 +2,7 @@
 <?php
 require('src/smtp.class.inc.php');
 
-$hp = new PHPSMTPServer;
-$hp->logFile      = '/var/www/php-smtp-server/mail.log';
-$hp->serverHello  = 'Mailroute.nl ESMTP Postfix';
-$hp->mailFile     = '/var/www/php-smtp-server/emails/'.uniqid('').'.mime';
-$hp->log(date('Y-m-d H:i:s') . ' RECEIVING NEW MESSAGE');
-$hp->receive();
-$hp->log(dirname(__FILE__));
+$fullpath = dirname(__FILE__);
+
+$hp = new PHPSMTPServer($fullpath);
 exit;
